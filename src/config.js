48c5d0582f271e69ff694243ef77161a52fc293d
YayaAreeyaDa-oh <areeyadaoh@gmail.com>
2020-03-19 20:47:45 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+import 'firebase/firestore'
+
+const config = {
+    apiKey: "AIzaSyDvqY3Ve5a5l5aYVwCmZG-2Q_LpID3RvHQ",
+    authDomain: "todo-311.firebaseapp.com",
+    databaseURL: "https://todo-311.firebaseio.com",
+    projectId: "todo-311",
+    storageBucket: "todo-311.appspot.com",
+    messagingSenderId: "626328720618",
+    appId: "1:626328720618:web:b9d97b68498be49e597393",
+    measurementId: "G-8WS501BMB0"
+  };
+
+  export default config;
